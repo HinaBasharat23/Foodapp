902c12861ac38512385cad7ccf50e5e82517a0d7
HinaBasharat23 <154341163+HinaBasharat23@users.noreply.github.com>
2023-12-19 06:57:29 -0800

    Add files via upload
    new file updated

---
+++ 2index.js
@@ -0,0 +1,28 @@
+let b =[1,2,3,4,5,6,7,8,9]
+for(let i of b){
+    console.log(i)
+}
+for(let j=0; j<=b.length; j++ ){
+    console.log(b[j]*2)
+}
+for(let j=0; j<=b.length; j++ ){
+    console.log(b[j]+2)
+}
+for(let j=0; j<=b.length; j++ ){
+    console.log(b[j]/2)
+}
+for(let j=0; j<=b.length; j++ ){
+    console.log(b[j]-2)
+}
+let h=parseInt(prompt("enter any number 1-10"))
+for(let k=0; k<=10; k++){
+    let result=h*k;
+    console.log(`${h} * ${k} = ${result}`)
+}
+functiontable(number)
+for(let i=0; i<=10;i++){
+    let result = i*number
+    console.log(`${number} * ${i} = ${result}`)
+}
+table(2)
+ 
---
+++ index.js
@@ -0,0 +1,59 @@
+let a = parseInt(prompt("enter days of week"))
+if(a==1){
+    console.log("today is monday")
+}
+if (a==2){
+    console.log("today is tuesday")
+}
+ if(a==3){
+    console.log("today is wednesday")
+}
+ if(a==4){
+    console.log("today is thursday")
+}
+ if(a==5){
+    console.log("today is friday")
+}
+ if(a==6){
+    console.log("today is saturday")
+}
+ if(a==7){
+    console.log("today is sunday")
+}
+let m = parseInt(prompt("enter names of months"))
+if(y==1){
+    console.log("its january")
+}
+if(y==2){
+    console.log("its febrary")
+}
+if(y==3){
+    console.log("its march")
+}
+if(y==4){
+    console.log("its april")
+}
+if(y==5){
+    console.log("its may")
+}
+if(y==6){
+    console.log("its june")
+}
+if(y==7){
+    console.log("its july")
+}
+if(y==8){
+    console.log("its august")
+}
+if(y==9){
+    console.log("its september")
+}
+if(y==10){
+    console.log("its october")
+}
+if(y==11){
+    console.log("its november")
+}
+if(y==12){
+    console.log("its december")
+}
